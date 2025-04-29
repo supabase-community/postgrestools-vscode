88ce9ab1eba604924b53c90bb4ab5d574061f517
Julian Domke <68325451+juleswritescode@users.noreply.github.com>
2025-04-29 09:35:02 +0200

    feat: copy latest server log file (#36)

--- src/commands.ts
+++ src/commands.ts
@@ -1,4 +1,4 @@
-import { window } from "vscode";
+import { Uri, window, workspace } from "vscode";
 import { downloadPglt, getDownloadedBinary } from "./downloader";
 import { restart, start, stop } from "./lifecycle";
 import { logger } from "./logger";
@@ -6,9 +6,12 @@ import { state } from "./state";
 import {
   clearGlobalBinaries,
   clearTemporaryBinaries,
+  dirExists,
   getVersion,
 } from "./utils";
 import { Releases } from "./releases";
+import { join } from "path";
+import os from "node:os";
 
 /**
  * These commands are exposed to the user via the Command Palette.
@@ -74,5 +77,132 @@ export class UserFacingCommands {
         `Using binary from "${session.binaryStrategyLabel}".`
       );
     }
+  }
+
+  static async copyLatestLogfile() {
+    logger.info("Starting to copy latest log file…");
+
+    let logdir;
+
+    if (process.env.PGT_LOG_PATH) {
+      logdir = Uri.file(process.env.PGT_LOG_PATH);
+    } else {
+      /*
+       * Looks are placed at different locations based on the platform.
+       * Linux: /home/alice/.cache/pgt
+       * Win: C:\Users\Alice\AppData\Local\supabase-community\pgt\cache
+       * Mac: /Users/Alice/Library/Caches/dev.supabase-community.pgt
+       */
+      switch (process.platform) {
+        case "darwin": {
+          logdir = Uri.file(
+            join(
+              os.homedir(),
+              "Library",
+              "Caches",
+              "dev.supabase-community.pgt"
+            )
+          );
+          break;
+        }
+        case "linux": {
+          logdir = Uri.file(join(os.homedir(), ".cache", "pgt"));
+          break;
+        }
+        case "win32": {
+          logdir = Uri.file(
+            join(
+              os.homedir(),
+              "AppData",
+              "Local",
+              "supabase-community",
+              "pgt",
+              "cache"
+            )
+          );
+          break;
+        }
+        default: {
+          window.showErrorMessage(
+            `Unsupported Platform: ${process.platform}. PostgresTools only runs on linux, darwin and windows.`
+          );
+          return;
+        }
+      }
+    }
+
+    logger.info("Determined log directory location", {
+      logdir: logdir.fsPath,
+    });
+
+    if (!(await dirExists(logdir))) {
+      window.showErrorMessage(`Did not find expected log directory.`);
+      return;
+    }
+
+    const logFiles = await workspace.fs
+      .readDirectory(logdir)
+      .then((files) => files.filter(([name]) => name.startsWith("server.log")));
+
+    if (logFiles.length === 0) {
+      window.showErrorMessage(`No log files found in log directory.`);
+      return;
+    }
+
+    logger.info(`Found ${logFiles.length} log files.`);
+
+    if (!state.activeProject) {
+      window.showErrorMessage(
+        `No active project, can't determine a location to copy the log file.`
+      );
+      return;
+    }
+
+    logFiles.sort((a, b) => {
+      const [dateA, idxA] = getDatePartsFromLogFile(a[0]);
+      const [dateB, idxB] = getDatePartsFromLogFile(b[0]);
+
+      if (dateA.getTime() === dateB.getTime()) {
+        return idxB - idxA;
+      } else {
+        return dateB.getTime() - dateA.getTime();
+      }
+    });
+
+    const [latestFilename] = logFiles[0];
+
+    logger.info(`Identified latest log file.`, {
+      filename: latestFilename,
+    });
+
+    try {
+      const target = Uri.joinPath(state.activeProject.path, "server.log");
+
+      await workspace.fs.copy(Uri.joinPath(logdir, latestFilename), target);
+
+      window.showInformationMessage(`Copied log file to ${target.fsPath}.`);
+    } catch (err) {
+      logger.error(`Error copying log file: ${err}`);
+      window.showErrorMessage(
+        `Error copying log file. View Output for more information.`
+      );
+    }
+  }
+}
+
+/** Expected format is server.log.2025-01-01-17 */
+function getDatePartsFromLogFile(filename: string): [Date, number] {
+  try {
+    const last = filename.split(".").pop()!;
+    const [year, month, day, idx] = last.split("-");
+
+    if (!year || !month || !day || !idx || Number.isNaN(+idx)) {
+      throw new Error();
+    }
+
+    return [new Date(`${year}-${month}-${day}`), +idx];
+  } catch {
+    logger.warn(`Unexpected log file name: ${filename}`);
+    return [new Date(0), 999];
   }
 }
